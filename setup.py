01fcdb054b20949dba09dece3fa00ddcbb82d988
L3Squad <l3squad.com@gmail.com>
2025-03-05 18:00:51 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="api_bridge",
-    version="0.1.0",
+    version="0.1.1",
     description="A FastAPI-based CRUD API generator for MySQL databases",
     author="Venkat.R",
     author_email="ai.venkat.r@gmail.com",
